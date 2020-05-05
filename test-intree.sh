9803095e395e9c657cb3e29b9fed3dfe5c303bc6
Robert E. Novak <sailnfool@gmail.com>
2020-05-05 16:02:28 -0700

    Fixed indentation

--- test-intree.sh
+++ test-intree.sh
@@ -3,10 +3,10 @@
 # Author Robert E. Novak
 # email: novak5@llnl.gov, sailnfool@gmail.com
 #
-# This script will set up in-tree testing of ZFS, taking care to 
+# This script will set up in-tree testing of ZFS, taking care to
 # install the necessary packages required for testing of ZFS
-# depending on the 
-# In the github wiki it recommends installing the following packages 
+# depending on the Linux installation
+# In the github wiki it recommends installing the following packages
 # for Ubuntu.
 # https://github.com/openzfs/zfs/wiki/Building-ZFS
 #
@@ -18,10 +18,10 @@ source func.errecho
 source func.debug
 
 USAGE="\r\n${0##*/} [-hn]\r\n
-\t\tTest an in-tree copy of ZFS.  The default is to retrieve a copy\r\n
+\t\tTest an in-tree copy of ZFS. The default is to retrieve a copy\r\n
 \t\tof the source tree from a repository, then give the user the\r\n
 \t\tchoice to select a branch from that repository for testing.\r\n
-\t\tThe default repository is a private fork.  The -s option\r\n
+\t\tThe default repository is a private fork.	The -s option\r\n
 \t\tgives the option to select the [standard] default OpenZFS\r\n
 \t\trepository and -r lets the user specify an alternate repository\r\n
 \t-h\t\tPrint this message\r\n
@@ -31,10 +31,10 @@ USAGE="\r\n${0##*/} [-hn]\r\n
 NUMARGS=0
 if [ "${NUMARGS}" -gt 0 ]
 then
-  if [ $# -lt "${NUMARGS}" ]
-  then
-    insufficient ${0##*/} ${LINENO} ${NUMARGS}
-  fi
+	if [ $# -lt "${NUMARGS}" ]
+	then
+		insufficient ${0##*/} ${LINENO} ${NUMARGS}
+	fi
 fi
 
 skip_get_branch=0
@@ -54,9 +54,9 @@ OS_REVISION=$(lsb_release -r | cut -f 2)
 host=$(hostname)
 if [ "${host}" = "slagi" ]
 then
-  ZFSPARENT="/tftpboot/global/novak5/github"
+	ZFSPARENT="/tftpboot/global/novak5/github"
 else
-  ZFSPARENT="$HOME/github"
+	ZFSPARENT="$HOME/github"
 fi
 mkdir -p ${ZFSPARENT}
 
@@ -67,35 +67,35 @@ errecho ${0##*/} ${LINENO} "Working with OS Release ${OS_REVISION}"
 optionargs="hn"
 while getopts ${optionargs} name
 do
-  case ${name} in
-    h)
-      errecho "-e" ${0##*/} ${LINENO} ${USAGE}
-      exit 0
-      ;;
-    n)
-      skip_get_branch=1
-      ;;
-    \?)
-      errecho "-e" ${0##*/} ${LINENO} "invalid option: -${OPTARG}"
-      errecho "-e" ${0##*/} ${LINENO} ${USAGE}
-      exit 1
-      ;;
-  esac
+	case ${name} in
+	h)
+		errecho "-e" ${0##*/} ${LINENO} ${USAGE}
+		exit 0
+		;;
+	n)
+		skip_get_branch=1
+		;;
+	\?)
+		errecho "-e" ${0##*/} ${LINENO} "invalid option: -${OPTARG}"
+		errecho "-e" ${0##*/} ${LINENO} ${USAGE}
+		exit 1
+		;;
+	esac
 done
 ####################
 # Get the correct version of the source from github
 # This small chunk of script will retrieve the list of branches
 # that are available for this repository and allow the user to
-# select which branch will be built for testing.  This next
-# section of code should probably be placed in a source'd 
+# select which branch will be built for testing. This next
+# section of code should probably be placed in a source'd
 # script since it is also used in "test-intree"
 ####################
 
 ZFSDIR="${ZFSPARENT}/zfs"
 if [ ! -d ${ZFSDIR} ]
 then
-  echo "Could not find ${ZFSDIR}"
-  exit 1
+	echo "Could not find ${ZFSDIR}"
+	exit 1
 fi
 errecho ${0##*/} $LINENO "Working with zfs in ${ZFSDIR}"
 cd ${ZFSDIR}
@@ -103,37 +103,37 @@ if [ "${skip_get_branch}" = 0 ]
 then
 	declare -A -g branch_name
 	git branch -a | \
-	  sed -n -e 's,remotes/origin/\([^H]\),\1,p' \
-	  > /tmp/zfs_branches.$$.txt
+	    sed -n -e 's,remotes/origin/\([^H]\),\1,p' > \
+	    /tmp/zfs_branches.$$.txt
 	branchnumber=1
 	while read branchname
 	do
-	  branch_name[${branchnumber}]=${branchname}
-    printf "%4d\t%s\n" ${branchnumber} ${branchname}
-    ((branchnumber++))
+		branch_name[${branchnumber}]=${branchname}
+		printf "%4d\t%s\n" ${branchnumber} ${branchname}
+		((branchnumber++))
 	done < /tmp/zfs_branches.$$.txt
 	read -p "Which Branch Number: " choice
 	rm -f /tmp/zfs_branches.$$.txt
-	
+
 	####################
 	# No matter the source, by default load the master branch,
 	# then load the selected branch.
 	####################
 	git checkout ${branch_name[$choice]}
-  new_branch=${branch_name[$choice]}
-  if [ -f $HOME/.zfs_last_branch ]
-  then
-    old_branch=$(cat $HOME/.zfs_last_branch)
-  else
-    echo "0" > $HOME/.zfs_last_branch
-  fi
-  if [ "${old_branch}" = "${new_branch}" ]
-  then
-    reconfigure=0
-  else
-    reconfigure=1
-    echo ${new_branch} > ~/.zfs_last_branch
-  fi
+	new_branch=${branch_name[$choice]}
+	if [ -f $HOME/.zfs_last_branch ]
+	then
+		old_branch=$(cat $HOME/.zfs_last_branch)
+	else
+		echo "0" > $HOME/.zfs_last_branch
+	fi
+	if [ "${old_branch}" = "${new_branch}" ]
+	then
+		reconfigure=0
+	else
+		reconfigure=1
+		echo ${new_branch} > ~/.zfs_last_branch
+	fi
 fi
 
 ####################
@@ -141,8 +141,8 @@ fi
 ####################
 if [ "${reconfigure}" -eq "1" ]
 then
-  sh autogen.sh
-  ./configure
+	sh autogen.sh
+	./configure
 fi
 make -s -j$(nproc)
 
@@ -154,43 +154,49 @@ make -s -j$(nproc)
 slagnames='slag.*'
 if [[ ! "${host}" =~ ${slagnames} ]]
 then
-  errecho ${0##*/} ${LINENO} \
-    "We are on host ${host} not a slag node"
+	errecho ${0##*/} ${LINENO} \
+		"We are on host ${host} not a slag node"
 	case ${OS_RELEASE} in
-	  Ubuntu | Debian)
-	    sudo apt install ksh bc fio acl sysstat mdadm lsscsi parted attr \
-	      dbench nfs-kernel-server samba rng-tools pax linux-tools-common \
-	      selinux-utils quota
-	    ;;
-	  RedHatEnterpriseServer | RedHatEnterpriseWorkstation )
-	
-	    ####################
-	    # WARNING!! WARNING!! WARNING!! Not yet tested
-	    # This needs a further test for RHEL 7 vs. RHEL 8
-	    ####################
-      version7name='7\..*'
-      version8name='8\..*'
-	    if [[ "${OS_REVISION}" =~ ${version7name} ]]
-	    then 
-	      sudo yum install ksh bc fio acl sysstat mdadm lsscsi parted \
-	        attr dbench nfs-utils samba rng-tools pax perf
-	    else
-	      if [[ "${OS_REVISION}" =~ ${version8name} ]]
-	      then
-	        sudo dnf install ksh bc fio acl sysstat mdadm lsscsi \
-	          parted attr dbench nfs-utils samba rng-tools pax perf
-	      fi
-	    fi
-	    ;;
-	  \?) #Invalid
-	    errecho "$-e" ${FUNCNAME} ${LINENO} \
-	      "Unknown Operating System ${OS_RELEASE}"
-	    exit 1
-	    ;;
+	Ubuntu | Debian)
+		sudo apt install ksh bc fio acl sysstat mdadm lsscsi \
+		    parted attr dbench nfs-kernel-server samba rng-tools \
+		    pax linux-tools-common selinux-utils quota
+		;;
+	RedHatEnterpriseWorkstation)
+
+		####################
+		# WARNING!! WARNING!! WARNING!! Not yet tested
+		# This needs a further test for RHEL 7 vs. RHEL 8
+		####################
+		version7name='7\..*'
+		version8name='8\..*'
+		if [[ "${OS_REVISION}" =~ ${version7name} ]]
+		then
+			sudo yum install ksh bc fio acl sysstat mdadm \
+			    lsscsi parted attr dbench nfs-utils samba \
+			    rng-tools pax perf
+		elif [[ "${OS_REVISION}" =~ ${version8name} ]]
+		then
+			sudo dnf install ksh bc fio acl sysstat mdadm \
+			    lsscsi parted attr dbench nfs-utils samba \
+			    rng-tools pax perf
+		fi
+		;;
+	RedHatEnterpriseServer)
+		####################
+		# Do Nothing for TOSS servers
+		####################
+		;;
+
+	\?) #Invalid
+		errecho "$-e" ${FUNCNAME} ${LINENO} \
+		    "Unknown Operating System ${OS_RELEASE}"
+		exit 1
+		;;
 	esac
 else
-  errecho ${0##*/} ${LINENO} \
-    "No update to tools required for zfs on TOSS"
+	errecho ${0##*/} ${LINENO} \
+		"No update to tools required for zfs on TOSS"
 fi
 #
 # zfs-helper.sh: Certain functionality (i.e. /dev/zvol/) depends
@@ -220,4 +226,3 @@ sudo ./scripts/zfs.sh
 #
 sudo ./scripts/zloop.sh
 # sudo ./scripts/zfs-tests.sh -vx
-# vim: set syntax=bash, ts=2, sw=2, lines=55, columns=120,colorcolumn=78
